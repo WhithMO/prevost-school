16fa096fd23f828ff85da5ec6c494f9ea3093d81
WhithMO <whithmann_98@hotmail.com>
2025-06-23 04:14:09 -0500

    Initial Commit

---
+++ src/main/java/com/colegio/prevost/PrevostSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.colegio.prevost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevostSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevostSchoolApplication.class, args);
+	}
+
+}
